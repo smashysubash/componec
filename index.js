50cbdfe9025d98dacb67f4520aad71c16e37db1d
Subash S <76615324+smashysubash@users.noreply.github.com>
2023-03-15 02:38:06 +0530

    intro added

--- index.js
+++ index.js
@@ -1,54 +1,28 @@
-window.onload=load();
 
-function load(){
-    document.querySelector(".bcknd").classList.toggle("animation");    
-}
-
-document.onreadystatechange = function() {
-    // document.querySelector(  "#body").style.visibility = "hidden";
-    // document.querySelector("#loader").style.visibility = "visible";
-    // setTimeout(hideanimate,0);
+var dismissLoadingScreen = function() {
+    var loader = document.getElementById("intro-container");
+    var bodycnt = document.getElementById("body");
     
-    if (document.readyState !== "complete") {
-        document.querySelector(
-          "body").style.visibility = "hidden";
-        document.querySelector(
-          "#loader").style.visibility = "visible";
-    } else {
-        document.querySelector(
-          "#loader").style.display = "none";
-        document.querySelector(
-          "body").style.visibility = "visible";
-          
-    }
+    document.getElementById("bgm").pause();
+    loader.style.display = "none";
+    bodycnt.style.display="block";
 };
-function hideanimate(){
+var executethis = function() {
+    console.log("hi");
+    var loader = document.getElementById("intro-container");
+    var firstpg = document.getElementById("firstpage");
     
-    document.querySelector("#loader").style.visibility = "hidden";
-    document.querySelector(  "#body").style.visibility = "visible";
-}
-
-var navL = document.querySelector('.nav-links');
-document.querySelector('.nav-btn').addEventListener('click',()=>{
-    if(navL.classList.contains('shrink')) {
-        navL.classList.remove('shrink');
-        navL.classList.add('unshrink');
-        
-    }else{
-        navL.classList.remove('unshrink');
-        navL.classList.add('shrink');
-    }
-}); 
-addEventListener("scroll", (event) => {});
-
-onscroll = (event) => {
-    if(navL.classList.contains('unshrink')) {
-        navL.classList.remove('unshrink');   
-        navL.classList.add('shrink');   
-    }
+    document.getElementById("bgm").play();
+    firstpg.style.display="none";
+    loader.style.display = "block";
+    var result = setTimeout(dismissLoadingScreen, 16000);
+};  
+var wait3seconds = function() {
+    var loader = document.getElementById("intro-container");
+    var bodycnt = document.getElementById("body");
+    
+    bodycnt.style.display="none";
+    loader.style.display="none";
 };
 
-
-// timer javascript
- 
- 
+window.addEventListener("load", wait3seconds);
