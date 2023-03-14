106918a5baa5825ef1b453efc69685e1e56f4d21
jeevansiddu <jeevansiddusb@gmail.com>
2023-03-14 21:32:45 +0530

    event content edited

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
-window.onload=load();
+window.onload = load();
 
-function load(){
-    document.querySelector(".bcknd").classList.toggle("animation");    
+function load() {
+  document.querySelector(".bcknd").classList.toggle("animation");
 }
 
-document.onreadystatechange = function() {
-    // document.querySelector(  "#body").style.visibility = "hidden";
-    // document.querySelector("#loader").style.visibility = "visible";
-    // setTimeout(hideanimate,0);
-    
-    if (document.readyState !== "complete") {
-        document.querySelector(
-          "body").style.visibility = "hidden";
-        document.querySelector(
-          "#loader").style.visibility = "visible";
-    } else {
-        document.querySelector(
-          "#loader").style.display = "none";
-        document.querySelector(
-          "body").style.visibility = "visible";
-          
-    }
+document.onreadystatechange = function () {
+  // document.querySelector(  "#body").style.visibility = "hidden";
+  // document.querySelector("#loader").style.visibility = "visible";
+  // setTimeout(hideanimate,0);
+
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
 };
-function hideanimate(){
-    
-    document.querySelector("#loader").style.visibility = "hidden";
-    document.querySelector(  "#body").style.visibility = "visible";
+function hideanimate() {
+  document.querySelector("#loader").style.visibility = "hidden";
+  document.querySelector("#body").style.visibility = "visible";
 }
 
-var navL = document.querySelector('.nav-links');
-document.querySelector('.nav-btn').addEventListener('click',()=>{
-    if(navL.classList.contains('shrink')) {
-        navL.classList.remove('shrink');
-        navL.classList.add('unshrink');
-        
-    }else{
-        navL.classList.remove('unshrink');
-        navL.classList.add('shrink');
-    }
-}); 
+var navL = document.querySelector(".nav-links");
+document.querySelector(".nav-btn").addEventListener("click", () => {
+  if (navL.classList.contains("shrink")) {
+    navL.classList.remove("shrink");
+    navL.classList.add("unshrink");
+  } else {
+    navL.classList.remove("unshrink");
+    navL.classList.add("shrink");
+  }
+});
 addEventListener("scroll", (event) => {});
 
 onscroll = (event) => {
-    if(navL.classList.contains('unshrink')) {
-        navL.classList.remove('unshrink');   
-        navL.classList.add('shrink');   
-    }
+  if (navL.classList.contains("unshrink")) {
+    navL.classList.remove("unshrink");
+    navL.classList.add("shrink");
+  }
 };
 
-
 // timer javascript
- 
- 
